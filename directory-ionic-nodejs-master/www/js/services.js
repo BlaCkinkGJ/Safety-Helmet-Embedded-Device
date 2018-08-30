3ef60a5be7eeca5750691be60faaa2094575f4a0
BlaCkinkGJ <ss5kijun@gmail.com>
2018-08-30 12:34:13 +0900

    NODE JS SYSTEM REMOVED
    NOW CANNOT USE THE NODE JS SYS

--- directory-ionic-nodejs-master/www/js/services.js
+++
@@ -1,4 +0,0 @@
-angular.module('directory.services', ['ngResource'])
-    .factory('Employees', function ($resource) {
-        return $resource('http://ionic-directory.herokuapp.com/employees/:employeeId/:data');
-    });
