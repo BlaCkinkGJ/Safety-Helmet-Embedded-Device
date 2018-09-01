1d1e394076d4b5c92cdc4086f9c33a7f0375dbc9
BlaCkinkGJ <ss5kijun@gmail.com>
2018-09-01 20:01:40 +0900

    디렉토리 정리
    쓸데 없는 것 전부 정리

--- raspberry/Embedded/sample/SSID.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-
-def getSSID():
-    args = ['/sbin/iwgetid', '-r']
-    proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-    (output, dummy) = proc.communicate()
-    return output
