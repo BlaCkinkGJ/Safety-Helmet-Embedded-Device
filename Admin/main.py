e33934e5a8c277ae46807cd04f0264341ef52f31
BlaCkinkGJ <ss5kijun@gmail.com>
2018-09-02 04:52:23 +0900

    실행 파일 제작
    실행파일 제작됨. 해당 파일을 타 PC에서도 실행시켜보도록 할 것

--- Admin/main.py
+++ Admin/main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 from res import signIn
 import sys
 from PyQt5 import QtWidgets, QtGui
@@ -32,27 +31,27 @@ def getAccount():
 
     return result
 
-if __name__ == "__main__":
-    pipe.info = getAccount()
-    pipe.app = QtWidgets.QApplication(sys.argv)
-    pipe.app.setWindowIcon(QtGui.QIcon('res/icon.png'))
+if __name__=='__main__':
+	pipe.info = getAccount()
+	pipe.app = QtWidgets.QApplication(sys.argv)
+	pipe.app.setWindowIcon(QtGui.QIcon('res/icon.ico'))
 
-    if pipe.info is None:
-        errMsg = QtWidgets.QErrorMessage()
-        errMsg.setFixedHeight(320)
-        errMsg.setFixedWidth(480)
-        errMsg.showMessage('res/data.xml이 없거나 data.xml 파일이 존재하지 않습니다.<br>'
-                           '이 경우 res 폴더에 data.xml을 아래와 같은 방식으로 만들어주십시오.<br>'
-                           '&lt;LoginData&gt;<br>'
-                           '&nbsp;&nbsp;&nbsp;&nbsp;&lt;ip&gt;데이터베이스 서버 아이피&lt;/ip&gt;<br>'
-                           '&nbsp;&nbsp;&nbsp;&nbsp;&lt;port&gt;포트&lt;/port&gt;<br>'
-                           '&nbsp;&nbsp;&nbsp;&nbsp;&lt;loginDB&gt;관리자 아이디 테이블 이름&lt;/loginDB&gt;<br>'
-                           '&nbsp;&nbsp;&nbsp;&nbsp;&lt;dataDB&gt;작업자 테이블 이름&lt;/dataDB&gt;<br>'
-                           '&lt;/LoginData&gt;<br>')
-    else:
-        pipe.db = DB.DB(ip=pipe.info['ip'], port=pipe.info['port'], table=pipe.info['loginDB'])
-        pipe.window = QtWidgets.QWidget()
-        pipe.ui = signIn.Ui_Form()
-        pipe.ui.setupUi(pipe.window)
-        pipe.window.show()
-    pipe.app.exec_()
+	if pipe.info is None:
+		errMsg = QtWidgets.QErrorMessage()
+		errMsg.setFixedHeight(320)
+		errMsg.setFixedWidth(480)
+		errMsg.showMessage('res/data.xml이 없거나 data.xml 파일이 존재하지 않습니다.<br>'
+						   '이 경우 res 폴더에 data.xml을 아래와 같은 방식으로 만들어주십시오.<br>'
+						   '&lt;LoginData&gt;<br>'
+						   '&nbsp;&nbsp;&nbsp;&nbsp;&lt;ip&gt;데이터베이스 서버 아이피&lt;/ip&gt;<br>'
+						   '&nbsp;&nbsp;&nbsp;&nbsp;&lt;port&gt;포트&lt;/port&gt;<br>'
+						   '&nbsp;&nbsp;&nbsp;&nbsp;&lt;loginDB&gt;관리자 아이디 테이블 이름&lt;/loginDB&gt;<br>'
+						   '&nbsp;&nbsp;&nbsp;&nbsp;&lt;dataDB&gt;작업자 테이블 이름&lt;/dataDB&gt;<br>'
+						   '&lt;/LoginData&gt;<br>')
+	else:
+		pipe.db = DB.DB(ip=pipe.info['ip'], port=pipe.info['port'], table=pipe.info['loginDB'])
+		pipe.window = QtWidgets.QWidget()
+		pipe.ui = signIn.Ui_Form()
+		pipe.ui.setupUi(pipe.window)
+		pipe.window.show()
+	pipe.app.exec_()
---
+++ Execution File/WIN32/main.spec
@@ -0,0 +1,31 @@
+# -*- mode: python -*-
+
+block_cipher = None
+
+
+a = Analysis(['main.py'],
+             pathex=['.\\test', 'D:\\System\\Downloads\\Admin'],
+             binaries=[],
+             datas=[],
+             hiddenimports=['PyQt5.sip', 'pandas._libs.tslibs.timedeltas', 'pandas._libs.tslibs.np_datetime', 'pandas._libs.tslibs.nattype', 'pandas._libs.skiplist'],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher)
+			 
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          name='main',
+          debug=False,
+          strip=False,
+          upx=True,
+          runtime_tmpdir=None,
+          console=False, 
+		  icon='D:\\System\\Downloads\\Admin\\res\\icon.ico')
