1cf3679c6f14a91523c0b558489623407e6f18dc
BlaCkinkGJ <ss5kijun@gmail.com>
2018-09-06 00:23:54 +0900

    status 수정
    issue에서 언급한 내용 수정

--- Admin/res/status.py
+++ Admin/res/status.py
@@ -354,6 +354,8 @@ class Ui_Form(object):
             dict[key] = []
         for row in data:
             for key, val in row.items():
+                if key == 'temper' and not isFloat(val):
+                    return
                 dict[key].append(val)
         self.dict = dict
 
